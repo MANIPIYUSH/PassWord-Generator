55822075ee57d0815f51335da62e86fb5d009b15
PIYUSH MANI CHAUBEY <42182675+MANIPIYUSH@users.noreply.github.com>
2022-10-27 12:24:35 +0530

    updated the feature

--- src/Component/Upperbox.js
+++ src/Component/Upperbox.js
@@ -4,14 +4,14 @@ import React from 'react'
 function Upperbox() {
   return (
    <div className='flex items-center justify-center mt-6'>
-     <div className='flex flex-col items-center justify-center border-2 w-96 h-56 '>
-      <div>
+     <div className='flex flex-col items-center justify-center border-2 w-96 h-56 shadow-[0_45px_70px_-25px_rgba(0,0,0,0.3)]'>
+      <div className='text-3xl'>
         PassWord Generator
       </div>
-      <div>
+      <div className='text-xl'>
         Yaha p Kuxx hoga
       </div>
-      <div className='border-solid border-2 border-indigo-600 h-8 w-64'>
+      <div className='border-solid border-2 border-indigo-600 h-8 w-64 mt-4'>
       
       </div>
       
